9e8e4e60762bca3985b9a98256137f8393a307c3
Moses Nova <mosesnova@gmail.com>
2020-10-15 13:05:18 +1300

    Initial Commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,73 +23,88 @@ namespace SignatureAnalysis
         public MainWindow()
         {
 
-            const double margin = 10;
-            double xmin = margin;
-            double xmax = canGraph.Width - margin;
-            double ymin = margin;
-            double ymax = canGraph.Height - margin;
-            const double step = 10;
+            //const double margin = 10;
+            //double xmin = margin;
 
-            // Make the X axis.
-            GeometryGroup xaxis_geom = new GeometryGroup();
-            xaxis_geom.Children.Add(new LineGeometry(
-                new Point(0, ymax), new Point(canGraph.Width, ymax)));
-            for (double x = xmin + step;
-                x <= canGraph.Width - step; x += step)
-            {
-                xaxis_geom.Children.Add(new LineGeometry(
-                    new Point(x, ymax - margin / 2),
-                    new Point(x, ymax + margin / 2)));
-            }
+            //double xmax = canGraph. - margin;
+            //double ymin = margin;
+            //double ymax = canGraph.Height - margin;
+            //const double step = 10;
 
-            Path xaxis_path = new Path();
-            xaxis_path.StrokeThickness = 1;
-            xaxis_path.Stroke = Brushes.Black;
-            xaxis_path.Data = xaxis_geom;
+            //// Make the X axis.
+            //GeometryGroup xaxis_geom = new GeometryGroup();
+            //xaxis_geom.Children.Add(new LineGeometry(
+            //    new Point(0, ymax), new Point(canGraph.Width, ymax)));
+            //for (double x = xmin + step;
+            //    x <= canGraph.Width - step; x += step)
+            //{
+            //    xaxis_geom.Children.Add(new LineGeometry(
+            //        new Point(x, ymax - margin / 2),
+            //        new Point(x, ymax + margin / 2)));
+            //}
 
-            canGraph.Children.Add(xaxis_path);
+            //Path xaxis_path = new Path();
+            //xaxis_path.StrokeThickness = 1;
+            //xaxis_path.Stroke = Brushes.Black;
+            //xaxis_path.Data = xaxis_geom;
 
-            // Make the Y ayis.
-            GeometryGroup yaxis_geom = new GeometryGroup();
-            yaxis_geom.Children.Add(new LineGeometry(
-                new Point(xmin, 0), new Point(xmin, canGraph.Height)));
-            for (double y = step; y <= canGraph.Height - step; y += step)
-            {
-                yaxis_geom.Children.Add(new LineGeometry(
-                    new Point(xmin - margin / 2, y),
-                    new Point(xmin + margin / 2, y)));
-            }
+            //canGraph.Children.Add(xaxis_path);
 
-            Path yaxis_path = new Path();
-            yaxis_path.StrokeThickness = 1;
-            yaxis_path.Stroke = Brushes.Black;
-            yaxis_path.Data = yaxis_geom;
+            //// Make the Y ayis.
+            //GeometryGroup yaxis_geom = new GeometryGroup();
+            //yaxis_geom.Children.Add(new LineGeometry(
+            //    new Point(xmin, 0), new Point(xmin, canGraph.Height)));
+            //for (double y = step; y <= canGraph.Height - step; y += step)
+            //{
+            //    yaxis_geom.Children.Add(new LineGeometry(
+            //        new Point(xmin - margin / 2, y),
+            //        new Point(xmin + margin / 2, y)));
+            //}
 
-            canGraph.Children.Add(yaxis_path);
+            //Path yaxis_path = new Path();
+            //yaxis_path.StrokeThickness = 1;
+            //yaxis_path.Stroke = Brushes.Black;
+            //yaxis_path.Data = yaxis_geom;
 
-            // Make some data sets.
-            Brush[] brushes = { Brushes.Red, Brushes.Green, Brushes.Blue };
-            Random rand = new Random();
-            for (int data_set = 0; data_set < 3; data_set++)
-            {
-                int last_y = rand.Next((int)ymin, (int)ymax);
+            //canGraph.Children.Add(yaxis_path);
 
-                PointCollection points = new PointCollection();
-                for (double x = xmin; x <= xmax; x += step)
-                {
-                    last_y = rand.Next(last_y - 10, last_y + 10);
-                    if (last_y < ymin) last_y = (int)ymin;
-                    if (last_y > ymax) last_y = (int)ymax;
-                    points.Add(new Point(x, last_y));
-                }
+            //// Make some data sets.
+            //Brush[] brushes = { Brushes.Red, Brushes.Green, Brushes.Blue };
+            //Random rand = new Random();
+            //for (int data_set = 0; data_set < 3; data_set++)
+            //{
+            //    int last_y = rand.Next((int)ymin, (int)ymax);
 
-                Polyline polyline = new Polyline();
-                polyline.StrokeThickness = 1;
-                polyline.Stroke = brushes[data_set];
-                polyline.Points = points;
+            //    PointCollection points = new PointCollection();
+            //    for (double x = xmin; x <= xmax; x += step)
+            //    {
+            //        last_y = rand.Next(last_y - 10, last_y + 10);
+            //        if (last_y < ymin) last_y = (int)ymin;
+            //        if (last_y > ymax) last_y = (int)ymax;
+            //        points.Add(new Point(x, last_y));
+            //    }
 
-                canGraph.Children.Add(polyline);
-            }
+            //    Polyline polyline = new Polyline();
+            //    polyline.StrokeThickness = 1;
+            //    polyline.Stroke = brushes[data_set];
+            //    polyline.Points = points;
+
+            //    canGraph.Children.Add(polyline);
+            //}
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Analysis Signature");
+            Valid();
+        }
+
+        private void Valid() {
+            MessageBox.Show("You Have Access");
+        }
+        private void InValid() {
+            MessageBox.Show("Access Denied");
+        }
+
     }
 }
