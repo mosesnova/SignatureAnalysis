0e52b4e3969cc99b893e84b0333e3fe80fac1074
Moses Nova <mosesnova@gmail.com>
2020-10-15 14:44:13 +1300

    Canvas and Matrices

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,81 +18,46 @@ namespace SignatureAnalysis
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    /// 
+
+    class Cinema
     {
-        public MainWindow()
+        private bool[,] seats = new bool[30, 15];
+        private const int size = 16;
+        private const int space = 2;
+
+        public void DrawRectangles(Canvas MyCanvas)
         {
+            for (int j = 0; j < seats.GetLength(1); j++)
+            {
+                for (int i = 0; i < seats.GetLength(0); i++)
+                {
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Height = size,
+                        Width = size,
+                    };
 
-            //const double margin = 10;
-            //double xmin = margin;
+                    rectangle.Fill = seats[i, j] ? Brushes.Red : Brushes.Green;
+                    MyCanvas.Children.Add(rectangle);
 
-            //double xmax = canGraph. - margin;
-            //double ymin = margin;
-            //double ymax = canGraph.Height - margin;
-            //const double step = 10;
-
-            //// Make the X axis.
-            //GeometryGroup xaxis_geom = new GeometryGroup();
-            //xaxis_geom.Children.Add(new LineGeometry(
-            //    new Point(0, ymax), new Point(canGraph.Width, ymax)));
-            //for (double x = xmin + step;
-            //    x <= canGraph.Width - step; x += step)
-            //{
-            //    xaxis_geom.Children.Add(new LineGeometry(
-            //        new Point(x, ymax - margin / 2),
-            //        new Point(x, ymax + margin / 2)));
-            //}
-
-            //Path xaxis_path = new Path();
-            //xaxis_path.StrokeThickness = 1;
-            //xaxis_path.Stroke = Brushes.Black;
-            //xaxis_path.Data = xaxis_geom;
-
-            //canGraph.Children.Add(xaxis_path);
-
-            //// Make the Y ayis.
-            //GeometryGroup yaxis_geom = new GeometryGroup();
-            //yaxis_geom.Children.Add(new LineGeometry(
-            //    new Point(xmin, 0), new Point(xmin, canGraph.Height)));
-            //for (double y = step; y <= canGraph.Height - step; y += step)
-            //{
-            //    yaxis_geom.Children.Add(new LineGeometry(
-            //        new Point(xmin - margin / 2, y),
-            //        new Point(xmin + margin / 2, y)));
-            //}
-
-            //Path yaxis_path = new Path();
-            //yaxis_path.StrokeThickness = 1;
-            //yaxis_path.Stroke = Brushes.Black;
-            //yaxis_path.Data = yaxis_geom;
-
-            //canGraph.Children.Add(yaxis_path);
-
-            //// Make some data sets.
-            //Brush[] brushes = { Brushes.Red, Brushes.Green, Brushes.Blue };
-            //Random rand = new Random();
-            //for (int data_set = 0; data_set < 3; data_set++)
-            //{
-            //    int last_y = rand.Next((int)ymin, (int)ymax);
-
-            //    PointCollection points = new PointCollection();
-            //    for (double x = xmin; x <= xmax; x += step)
-            //    {
-            //        last_y = rand.Next(last_y - 10, last_y + 10);
-            //        if (last_y < ymin) last_y = (int)ymin;
-            //        if (last_y > ymax) last_y = (int)ymax;
-            //        points.Add(new Point(x, last_y));
-            //    }
-
-            //    Polyline polyline = new Polyline();
-            //    polyline.StrokeThickness = 1;
-            //    polyline.Stroke = brushes[data_set];
-            //    polyline.Points = points;
-
-            //    canGraph.Children.Add(polyline);
-            //}
+                    Canvas.SetLeft(rectangle, i * (size + space));
+                    Canvas.SetTop(rectangle, j * (size + space));
+                }
+            }
         }
 
+    }
+    public partial class MainWindow : Window
+    {
+        private Cinema cinema = new Cinema();
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            cinema.DrawRectangles(MyCanvas);
+        }
+       
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Analysis Signature");
